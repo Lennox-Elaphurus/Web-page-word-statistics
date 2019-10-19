4768bce9310341091da7f946ade2020aaaa9c069
Lennox-VSCode <43885619+Lennox-Elaphurus@users.noreply.github.com>
2019-10-19 19:20:06 +0800

    v2.0

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import re
 import urllib.request
+import signal                # for handling KeyboardInterrupts
 
+def save():
+    global totalWordsCnt
+    global uniqueWordsCnt
+    global wordList
+    recordFile=open("record.txt",'w')
+    recordFile.write("totalWordsCnt "+str(totalWordsCnt)+" uniqueWordsCnt "+str(uniqueWordsCnt))
+    for key in wordList:
+        word=key
+        word=word.replace("'","")
+        word =word.replace(",","")
+        word = word.replace("[", "")
+        word = word.replace("]", "")
+        # print(word)
+        recordFile.write("\n"+word+" "+str(wordList[key]))
+    recordFile.close()
+    print("Record saved: "+"totalWordsCnt "+str(totalWordsCnt)+" uniqueWordsCnt "+str(uniqueWordsCnt))
+
+def handle_SIGINT(signal, frame) :
+    save()
+    print("Keyboard operation detected! Exit creeping.")
+
+
+signal.signal(signal.SIGINT, handle_SIGINT)
 # import record
 recordFile=open("record.txt",'r')
 wordList={}
@@ -16,50 +40,37 @@ else:
 
 print("file imported: totalWordsCnt = "+str(totalWordsCnt)+" uniqueWordsCnt = "+str(uniqueWordsCnt))
 # finish import record
+creepingCnt=0
+while(1):
+    if creepingCnt%10==0:
+        print("Creeping times: "+str(creepingCnt))
+        save()
+    response = urllib.request.urlopen('https://en.wikibooks.org/wiki/Special:Random')
+    html = response.read()
+    # getText
+    formattedData=[]
+    pattern = re.compile("\s+(?P<content>[a-zA-Z_]+)\s+", re.DOTALL)
+    contentMatch = re.findall(pattern, str(html))
+    # print(contentMatch)
+    contentMatch =str(contentMatch).split()
+    # finish getting words
 
-response = urllib.request.urlopen('https://en.wikibooks.org/wiki/Special:Random')
-html = response.read()
-# getText
-formattedData=[]
-pattern = re.compile("\s+(?P<content>[a-zA-Z_]+)\s+", re.DOTALL)
-# (<p>)|(<dd>)|(<li>)
-contentMatch = re.findall(pattern, str(html))
-print(contentMatch)
-contentMatch =str(contentMatch).split()
-# finish getting words
+    i=4
+    while i<=len(records)-2 :
+        wordList[records[i]]=int(records[i+1])
+        i=i+2
 
+    for content in contentMatch:
+        if content in wordList.keys() and content is not "totalWords":
+            wordList[content]=wordList[content]+1
+        else:
+            wordList[content]=1
+            uniqueWordsCnt=uniqueWordsCnt+1
+        totalWordsCnt = totalWordsCnt + 1
+    # finish counting
+    creepingCnt=creepingCnt+1
 
-
-
-i=4
-while i<=len(records)-2 :
-    wordList[records[i]]=int(records[i+1])
-    i=i+2
-
-for content in contentMatch:
-    if content in wordList.keys() and content is not "totalWords":
-        wordList[content]=wordList[content]+1
-    else:
-        wordList[content]=1
-        uniqueWordsCnt=uniqueWordsCnt+1
-totalWordsCnt = totalWordsCnt + int(len(contentMatch))
-# finish counting
-def save(totalWordsCnt,uniqueWordsCnt,wordList):
-
-    recordFile=open("record.txt",'w')
-    recordFile.write("totalWordsCnt "+str(totalWordsCnt)+" uniqueWordsCnt "+str(uniqueWordsCnt))
-    for key in wordList:
-        word=key
-        word=word.replace("'","")
-        word =word.replace(",","")
-        word = word.replace("[", "")
-        word = word.replace("]", "")
-        print(word)
-        recordFile.write("\n"+word+" "+str(wordList[key]))
-    recordFile.close()
-
-
-save(totalWordsCnt,uniqueWordsCnt,wordList)
+save()
 # fp   = open("text.html", 'wb')
 # fp.write(html)
 # fp.close()
