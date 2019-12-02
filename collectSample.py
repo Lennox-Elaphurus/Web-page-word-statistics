4b621031261c2978fcbe1cb9d6c0a1faa6631cc3
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2019-12-02 14:53:38 +0800

    Update collectSample.py

--- collectSample.py
+++ collectSample.py
@@ -17,9 +17,10 @@ def crawlingSample(NO):
     except:
         os.makedirs("sample")
     try:
+        text=""
         while terminate == "False":
             cntA+=1
-            print(cntA)
+            print(processMessage+"Creeping "+cntA)
             # https://en.wikibooks.org/wiki/Special:Random
             # https://en.wikibooks.org/api/rest_v1/page/random/summary
             try:
@@ -32,13 +33,14 @@ def crawlingSample(NO):
                 continue
             else:
                 soup = BeautifulSoup(response, 'html.parser')
-                text=soup.get_text()
+                text=soup.get_text()+text
 
             if cntA%100==0:
                 currentTime = str(time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time())))
                 fp   = open("sample/"+currentTime+".txt", 'w',encoding="utf-8")
                 fp.write(text)
                 fp.close()
+                text=""
                 print(currentTime+".txt Saved.")
     except KeyboardInterrupt:
         print(processMessage+"Detected keyboard interrupt in process "+str(NO)+" .")
@@ -52,27 +54,27 @@ def crawlingSample(NO):
     # fp.close()
 
 
-#_________________________main________________________________
-# if __name__=='__main__':
-#     # very necessary when your are in windows
-#     # multiprocessing.freeze_support()
-#
-#     print('Parent process %s.' % os.getpid())
-#     processCnt,terminate=getConfig()
-#     # maintain()
-#
-#     pool = Pool(processes=processCnt)
-#     for i in range(processCnt):
-#         pool.apply_async(crawlingSample,args=(i,))
-#
-#     print('Waiting for all subprocesses done...')
-#     pool.close()
-#     pool.join()
-#     print('All subprocesses done.')
-#     time.sleep(5)
-# else:
-#     print("In child process.")
-#     time.sleep(10)
-#     exit(-1)
+# _________________________main________________________________
+if __name__=='__main__':
+    # very necessary when your are in windows
+    multiprocessing.freeze_support()
 
-crawlingSample(-1)
+    print('Parent process %s.' % os.getpid())
+    processCnt,terminate=getConfig()
+    # maintain()
+
+    pool = Pool(processes=processCnt)
+    for i in range(processCnt):
+        pool.apply_async(crawlingSample,args=(i,))
+
+    print('Waiting for all subprocesses done...')
+    pool.close()
+    pool.join()
+    print('All subprocesses done.')
+    time.sleep(5)
+else:
+    print("In child process.")
+    time.sleep(10)
+    exit(-1)
+
+# crawlingSample(-1)
